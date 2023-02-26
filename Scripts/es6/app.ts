40d94acb4070ba80d84d8d3e9777b152df2eceab
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-02-26 16:24:10 +0400

    Adding the conflic attributes

--- Scripts/es6/app.ts
+++ Scripts/es6/app.ts
@@ -8,7 +8,31 @@ export default class app {
 
     constructor() {
         $(() => {
-            $('#example').DataTable();
+            $('#example').DataTable({
+                fixedHeader: true,
+                responsive: true,
+                searchHighlight: true,
+                filter: 'applied',
+                fixedColumns: true,
+                searching: true,
+                paging: false,
+                order: [[1, 'asc']],
+                columns: [
+
+                    { orderable: false, width: '4.5%' },
+                    { orderable: true, width: '5%' },
+                    { orderable: true, width: '24%' },
+                    { orderable: true, width: '2.5%' },
+                    { orderable: true, width: '10%' },
+                    { orderable: true, width: '9%' },
+                    { orderable: false, width: '5%' },
+                    { orderable: false, width: '25%' },
+                    { orderable: true, width: '10%' },
+                    { orderable: true, width: '5%' }
+
+                ],
+                autoWidth: false
+            });
         });
     }
 }
